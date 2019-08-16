64d7417ec6ee041543e8dbb06bdef3459c217dea
Santamarian Bogdan <santamarian.bogdan@gmail.com>
2019-08-16 16:41:52 +0300

    Added MVC

---
+++ BitcoinTicker/Model/BitcoinDataModel.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinDataModel.swift
+//  BitcoinTicker
+//
+//  Created by Dan Pop on 16/08/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class BitcoinDataModel {
+    
+    var bitcoinPrice: Double = 0
+    var lowPrice: Double = 0
+    var highPrice: Double = 0
+    
+}
